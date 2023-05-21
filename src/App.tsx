5ae368c64902b5bb9a5093e4ca02d7b19d5cf8d6
Gileade Kelvin <gileadekelvin@gmail.com>
2023-05-21 08:57:09 -0300

    feat: config ui.shadcn library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-function App() {
+import { Button } from '@/components/ui/button';
+
+const App = () => {
   return (
-    <main className='flex h-screen items-center justify-center'>
-      <h1 className='text-5xl font-bold'>Weather tree</h1>
+    <main className='flex h-screen flex-col items-center justify-center gap-4'>
+      <h1 className='text-5xl font-bold'>Enterobacteriaceae</h1>
+      <Button>Button</Button>
     </main>
   );
-}
+};
 
 export default App;
