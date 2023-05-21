b2f8bb81f6b3e22734eba7db1a099d273b456b4a
Gileade Kelvin <gileadekelvin@gmail.com>
2023-05-21 08:39:28 -0300

    chore: init vite (react+typescript), prettier and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className='flex h-screen items-center justify-center'>
+      <h1 className='text-5xl font-bold'>Weather tree</h1>
+    </main>
+  );
+}
+
+export default App;
